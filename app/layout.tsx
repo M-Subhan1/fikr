f018059adfb1c16e16b050d71a7d1f2c0606f64c
subaru <m.subhanabbas@gmail.com>
2024-12-23 12:19:09 +0500

    feat: configure fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const arahama = localFont({
+  src: "./fonts/ara-hama-al-fida.ttf",
+  display: "swap",
+  variable: "--font-arahama",
 });
 
 export const metadata: Metadata = {
@@ -24,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${arahama.variable}`}>
+      <body className="antialiased font-arahama">{children}</body>
     </html>
   );
 }
